c108a8f136023fbdef45b91e9f7614d1840d4b52
xoich <parisidaniele@protonmail.com>
2020-07-17 15:53:29 +0200

    implement phase 2

--- server.lua
+++ server.lua
@@ -14,20 +14,22 @@ local x509chain = require "openssl.x509.chain"
 local b64 = require "b64"
 local fmt = string.format
 
-local usetls = true
+local usetls = arg[1] ~= "dev"
 local tlschainpath = "fullchain.pem"
 local tlskeypath = "privkey.pem"
 local port = 8888
-local clientid = "1234567890client"
-local clientpass = "clientpass"
-local deviceid = "1234567890device"
-local devicekey = {0x0c, 0xc0, 0x52, 0xf6, 0x7b, 0xbd, 0x05, 0x0e, 0x75, 0xac, 0x0d, 0x43, 0xf1, 0x0a, 0x8f, 0x35}
-devicekey = string.pack(string.rep("B",16), table.unpack(devicekey))
-
 local cq = cqueues.new()
 local tlsctx = nil
 
-if usetls then
+local tickets = {}
+
+local testclientid = "1234567890client"
+local testclientpass = "clientpass"
+local testdeviceid = "1234567890device"
+local testdevicekey = {0x0c, 0xc0, 0x52, 0xf6, 0x7b, 0xbd, 0x05, 0x0e, 0x75, 0xac, 0x0d, 0x43, 0xf1, 0x0a, 0x8f, 0x35}
+testdevicekey = string.pack(string.rep("B",16), table.unpack(testdevicekey))
+
+if usetls then -- setup TLS
   tlsctx = sslcontext.new("TLS", true)
   local keyf = assert(io.open(tlskeypath))
   local key = keyf:read("a")
@@ -56,52 +58,156 @@ if usetls then
   tlsctx:setPrivateKey(pkey)
 end
 
-local function make_otp(operation, key, nonce1)
-  local t = {
-    OP = operation,
-    N1 = nonce1,
-    N2 = string.format("%u", (string.unpack("I", rand.bytes(4))))
-  }
-  local plain = cjson.encode(t)
-  plain = plain .. string.rep(' ', 16 - (#plain % 16))
+local function auth_decrypt(message, key)
+  assert(#message >= 64, "load too small")
+  local iv_blocks = string.sub(message, 1, -33)
+  local messagehmac = string.sub(message, -32)
+  assert(hmac.new(key, "sha256"):final(iv_blocks) == messagehmac, "invalid HMAC")
+  local iv = string.sub(iv_blocks, 1, 16)
+  local ciphertext = string.sub(iv_blocks, 17, -1)
+  local aes = cipher.new("AES-128-CBC")
+  aes:decrypt(key, iv, false)
+  return aes:final(ciphertext)
+end
+
+local function auth_encrypt(plain, key)
+  local rem = #plain % 16
+  plain = plain .. string.rep(' ', rem == 0 and 0 or 16 - rem)
   local aes = cipher.new("AES-128-CBC")
   local iv = rand.bytes(16)
   aes:encrypt(key, iv, false)
   local ivaes = iv .. aes:final(plain)
-  return b64.encode(ivaes .. hmac.new(key, "sha256"):final(ivaes))
+  return ivaes .. hmac.new(key, "sha256"):final(ivaes)
 end
 
+local function make_confirmation(N3)
+  local t = {
+    N3 = N3
+  }
+  return b64.encode(auth_encrypt(cjson.encode(t), key))
+end
+
+local function make_authopmsg(operation, key, N1, N2)
+  local t = {
+    OP = operation,
+    N1 = N1,
+    N2 = N2
+  }
+  return b64.encode(auth_encrypt(cjson.encode(t), key))
+end
+
+local function client_error(stream, res_headers)
+  res_headers:upsert(":status", "400")
+  assert(stream:write_headers(res_headers, true))
+end
+
+local function safe_decode_load(dload)
+  assert(#dload < 500, "device load too large")
+  assert(not string.find(dload, "[^a-zA-Z0-9/%+=]"), "invalid base64 device load")
+  dload = b64.decode(dload)
+  local loadlen = #dload
+  dload = string.sub(dload, 1, loadlen - (loadlen % 16))
+  return dload
+end
+
+local function authenticate_client(clientid, clientpass)
+  return clientpass == testclientpass and clientid == testclientid
+end
+
+local function authorize_client(clientid, deviceid, operation)
+  return clientid == testclientid and deviceid == testdeviceid and
+    (operation == "lock" or operation == "unlock")
+end
+
+local function get_device_key(deviceid)
+  if deviceid == testdeviceid then
+    return testdevicekey
+  else
+    error("deviceid not found")
+  end
+end
+
+local function record_authorization(ticket, N2, clientid, deviceid, operation)
+  tickets[ticket] = {
+    N2 = N2, clientid = clientid, deviceid = deviceid, operation = operation, ts = os.time()
+  }
+end
+
+local function get_ticketdata(ticket)
+  local ticketdata = tickets[ticket]
+  if ticketdata and (os.time() - ticketdata.ts) > 120 then
+    return nil
+  else
+    return ticketdata
+  end
+end
 local function authorize_operation_handler(stream, res_headers)
+  local body = stream:get_body_as_string()
+  if not body then return client_error(stream, res_headers) end
+  local json = cjson.decode(body)
+  if not json then return client_error(stream, res_headers) end
+  local clientid = json.client_id
+  local clientpass = json.client_pass
+  local deviceid = json.device_id
+  local operation = json.operation
+  local deviceload = json.load
+  if not (clientid and deviceid and operation and deviceload and clientpass) then
+    return client_error(stream, res_headers)
+  end
+  if not authenticate_client(clientid, clientpass) then
+    res_headers:upsert(":status", "403")
+    assert(stream:write_headers(res_headers, true))
+    return
+  end
+  if not authorize_client(clientid, deviceid, operation) then
+    res_headers:upsert(":status", "403")
+    assert(stream:write_headers(res_headers, true))
+    return
+  end
+  local binload = safe_decode_load(deviceload)
+  local key = get_device_key(deviceid)
+  local devjson = assert(cjson.decode(auth_decrypt(binload, key)))
+  local N1 = devjson.N1
+  assert(N1)
+  local ticket = string.format("%x", (string.unpack("l", rand.bytes(8))))
+  local N2 = string.format("%x", (string.unpack("l", rand.bytes(8))))
+  record_authorization(ticket, N2, clientid, deviceid, operation)
+  local aOP = make_authopmsg(operation, key, N1, N2)
+  local body = cjson.encode({ success = true, load = aOP, ticket = ticket })
   res_headers:append(":status", "200")
   res_headers:append("content-type", "application/json")
-  local body = assert(stream:get_body_as_string())
-  local json = assert(cjson.decode(body))
-  local reqclientid = json.client_id
-  local reqoperation = json.operation
-  local loadb64 = json.load
-  assert(json.device_id == deviceid, "client not found")
-  assert(reqclientid == clientid, "client not found")
-  assert(json.client_pass == clientpass, "invalid client password")
-  assert(reqoperation == "lock" or reqoperation == "unlock", "invalid operation")
-  assert(not string.find(loadb64, "[^a-zA-Z0-9/%+=]"), "invalid base64 load")
-  local loadraw = b64.decode(loadb64)
-  loadraw = string.sub(loadraw, 1, #loadraw - (#loadraw % 16))
-  assert(#loadraw >= 64, "load too small")
-  local loadcbc = string.sub(loadraw, 1, -33)
-  local loadhmac = string.sub(loadraw, -32)
-  assert(hmac.new(devicekey, "sha256"):final(loadcbc) == loadhmac, "invalid HMAC")
-  local iv = string.sub(loadcbc, 1, 16)
-  local ciphertext = string.sub(loadcbc, 17, -1)
-  local aes = cipher.new("AES-128-CBC")
-  aes:decrypt(devicekey, iv, false)
-  local message1 = aes:final(ciphertext)
-  local message1 = assert(cjson.decode(message1), "invalid device message")
-  local nonce1 = message1.N1
-  assert(nonce1, "Nonce 1 not provided")
-  local otp = make_otp(reqoperation, devicekey, nonce1)
-  local t = { success = true, otp = otp }
   assert(stream:write_headers(res_headers, false))
-  assert(stream:write_chunk(cjson.encode(t), true))
+  assert(stream:write_chunk(body, true))
+end
+
+local function confirm_operation_handler(stream, res_headers)
+  local body = stream:get_body_as_string()
+  if not body then return client_error(stream, res_headers) end
+  local json = cjson.decode(body)
+  if not json then return client_error(stream, res_headers) end
+  local ticket = json.ticket
+  local deviceload = json.load
+  if not (ticket and deviceload) then
+    return client_error(stream, res_headers)
+  end
+  local ticketdata = get_ticketdata(ticket)
+  if not get_ticket(ticketdata) then
+    res_headers:upsert(":status", "403")
+    assert(stream:write_headers(res_headers, true))
+    return
+  end
+  local binload = safe_decode_load(deviceload)
+  local key = get_device_key(ticketdata.deviceid)
+  local devjson = assert(cjson.decode(auth_decrypt(binload, key)))
+  local N2 = devjson.N2
+  local N3 = devjson.N3
+  assert(N2 == ticketdata.N2, "Invalid nonce received during confirmation")
+  local confirmation = make_confirmation(N3)
+  local body = cjson.encode({ success = true, load = confirmation })
+  res_headers:append(":status", "200")
+  res_headers:append("content-type", "application/json")
+  assert(stream:write_headers(res_headers, false))
+  assert(stream:write_chunk(body, true))
 end
 
 local myserver =
@@ -116,14 +222,17 @@ local myserver =
              local req_method = req_headers:get ":method"
 
              local res_headers = http_headers.new()
-             if req_method ~= "POST" and req_method ~= "HEAD" then
+             if req_method ~= "POST" then
                res_headers:upsert(":status", "405")
                assert(stream:write_headers(res_headers, true))
                return
              end
-             if req_headers:get ":path" == "/authorize-operation"
-             and req_headers:get "content-type" == "application/json" then
+             if req_headers:get(":path") == "/authorize-operation" and
+             req_headers:get "content-type" == "application/json" then
                return authorize_operation_handler(stream, res_headers)
+             elseif req_headers:get(":path") == "/confirm-operation" and
+             req_headers:get "content-type" == "application/json" then
+               return confirm_operation_handler(stream, res_headers)
              else
                res_headers:append(":status", "404")
                assert(stream:write_headers(res_headers, true))
